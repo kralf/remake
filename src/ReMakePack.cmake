ed6ed42f168a8b4e114cc51dd76963dd94e06b9e
Ralf Kaestner <ralf.kaestner@gmail.com>
2009-11-25 09:30:25 +0000

    Added package uninstall targets to ReMakePack

--- src/ReMakePack.cmake
+++ src/ReMakePack.cmake
@@ -27,15 +27,17 @@ include(ReMakePrivate)
 remake_set(REMAKE_PACK_ALL_TARGET packages)
 remake_set(REMAKE_PACK_TARGET_SUFFIX package)
 remake_set(REMAKE_PACK_INSTALL_ALL_TARGET packages_install)
+remake_set(REMAKE_PACK_UNINSTALL_ALL_TARGET packages_uninstall)
 remake_set(REMAKE_PACK_INSTALL_TARGET_SUFFIX package_install)
+remake_set(REMAKE_PACK_UNINSTALL_TARGET_SUFFIX package_uninstall)
 
 remake_set(REMAKE_PACK_DIR ReMakePackages)
 remake_set(REMAKE_PACK_SOURCE_DIR ReMakeSourcePackages)
 
 ### \brief Generate packages from a ReMake project.
-#   This macro generally configures package generation for a ReMake project 
+#   This macro generally configures package generation for a ReMake project
 #   using CMake's CPack macros. It gets invoked by the generator-specific
-#   macros defined in this module and should not be called directly from a 
+#   macros defined in this module and should not be called directly from a
 #   CMakeLists.txt file. The macro creates a package build target from the
 #   specified install component and initializes the CPack variables.
 #   \required[value] generator The generator to be used for creating the
@@ -92,27 +94,30 @@ endmacro(remake_pack)
 #   This macro configures package generation using CPack's DEB generator
 #   for Debian packages. It acquires all the information necessary from
 #   the current project settings and the arguments passed. In addition to
-#   creating a package build target through remake_pack(), the macro adds a 
+#   creating a package build target through remake_pack(), the macro adds a
 #   simplified package install target.
 #   \optional[value] ARCH:architecture The package architecture that is
 #     inscribed into the package manifest, defaults to the local system
 #     architecture as returned by 'dpkg --print-architecture'.
 #   \optional[value] COMPONENT:component The name of the install component to
 #     generate the Debian package from, defaults to ${REMAKE_PROJECT_COMPONENT}.
-#     Note that following Debian conventions, the component name is used as 
+#     Note that following Debian conventions, the component name is used as
 #     suffix to the package name. However, a component name matching
 #     ${REMAKE_PROJECT_COMPONENT} results in an empty suffix.
 #   \optional[list] dep An optional list of package dependencies
-#     that are inscribed into the package manifest. The format of a 
+#     that are inscribed into the package manifest. The format of a
 #     dependency should comply to Debian conventions, meaning that the
 #     dependency is of the form ${PACKAGE} [(>= ${VERSION})].
 macro(remake_pack_deb)
-  remake_arguments(PREFIX pack_ VAR ARCH VAR COMPONENT ARGN dependencies 
+  remake_arguments(PREFIX pack_ VAR ARCH VAR COMPONENT ARGN dependencies
     ${ARGN})
 
   if(NOT TARGET ${REMAKE_PACK_INSTALL_ALL_TARGET})
     remake_target(${REMAKE_PACK_INSTALL_ALL_TARGET})
   endif(NOT TARGET ${REMAKE_PACK_INSTALL_ALL_TARGET})
+  if(NOT TARGET ${REMAKE_PACK_UNINSTALL_ALL_TARGET})
+    remake_target(${REMAKE_PACK_UNINSTALL_ALL_TARGET})
+  endif(NOT TARGET ${REMAKE_PACK_UNINSTALL_ALL_TARGET})
 
   execute_process(COMMAND dpkg --print-architecture
     OUTPUT_VARIABLE pack_deb_arch OUTPUT_STRIP_TRAILING_WHITESPACE)
@@ -125,7 +130,7 @@ macro(remake_pack_deb)
     remake_set(pack_prefix ${pack_component})
     remake_set(pack_suffix ${pack_component})
   endif(pack_component STREQUAL REMAKE_PROJECT_COMPONENT)
-  
+
   if(pack_suffix)
     remake_file_name(pack_name ${REMAKE_PROJECT_FILENAME}-${pack_suffix})
     remake_file_name(pack_file ${REMAKE_PROJECT_FILENAME}-${pack_suffix}
@@ -151,4 +156,11 @@ macro(remake_pack_deb)
     COMMENT "Installing ${pack_name} package")
   add_dependencies(${pack_install_target} ${pack_target})
   add_dependencies(${REMAKE_PACK_INSTALL_ALL_TARGET} ${pack_install_target})
+
+  remake_target_name(pack_uninstall_target ${pack_prefix}
+    ${REMAKE_PACK_UNINSTALL_TARGET_SUFFIX})
+  remake_target(${pack_uninstall_target}
+    COMMAND sudo dpkg --remove ${pack_name}
+    COMMENT "Uninstalling ${pack_name} package")
+  add_dependencies(${REMAKE_PACK_UNINSTALL_ALL_TARGET} ${pack_uninstall_target})
 endmacro(remake_pack_deb)
