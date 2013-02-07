fd3e01358136f2b8250a844f3f530d39ce6f936e
Ralf Kaestner <ralf.kaestner@gmail.com>
2013-02-07 09:48:42 +0000

    New features: + remake_ros_find_package() delivers library directories + remake_pack_archive() is a new macros to create binary archives + remake_pack_source() and remake_pack_source_archive() are new macros for creating source archives

--- src/ReMakePack.cmake
+++ src/ReMakePack.cmake
@@ -24,8 +24,9 @@ include(ReMakePrivate)
 #   The ReMake packaging macros have been designed to provide simple and
 #   transparent package generation using CMake's CPack module.
 
-remake_set(REMAKE_PACK_ALL_TARGET packages)
-remake_set(REMAKE_PACK_TARGET_SUFFIX package)
+remake_set(REMAKE_PACK_ALL_BINARY_TARGET packages)
+remake_set(REMAKE_PACK_ALL_SOURCE_TARGET source_packages)
+remake_set(REMAKE_PACK_BINARY_TARGET_SUFFIX package)
 remake_set(REMAKE_PACK_INSTALL_ALL_TARGET packages_install)
 remake_set(REMAKE_PACK_INSTALL_TARGET_SUFFIX package_install)
 remake_set(REMAKE_PACK_UNINSTALL_ALL_TARGET packages_uninstall)
@@ -34,27 +35,30 @@ remake_set(REMAKE_PACK_UNINSTALL_TARGET_SUFFIX package_uninstall)
 remake_set(REMAKE_PACK_DIR ReMakePackages)
 remake_set(REMAKE_PACK_SOURCE_DIR ReMakeSourcePackages)
 
-### \brief Generate packages from a ReMake project.
-#   This macro generally configures package generation for a ReMake project
-#   using CMake's CPack macros. It gets invoked by the generator-specific
-#   macros defined in this module and should not be called directly from a
-#   CMakeLists.txt file. The macro creates a package build target from the
-#   specified install component and initializes the CPack variables.
+### \brief Generate binary packages from a ReMake project.
+#   This macro generally configures binary package generation for a
+#   ReMake project using CMake's CPack macros. It gets invoked by the
+#   generator-specific macros defined in this module and should not be
+#   called directly from a CMakeLists.txt file. The macro creates a
+#   package build target from the specified install component and
+#   initializes the CPack variables.
 #   \required[value] generator The generator to be used for creating the
-#     component package. See the CPack documentation for valid generators.
-#   \optional[value] NAME:name The name of the package to be generated,
-#     defaults to the ReMake project name.
-#   \optional[value] COMPONENT:component The name of the install component
-#     to generate the package from, defaults to ${REMAKE_DEFAULT_COMPONENT}.
-macro(remake_pack pack_generator)
+#     binary component package. See the CPack documentation for valid
+#     generators.
+#   \optional[value] NAME:name The name of the binary package to be
+#     generated, defaults to the ReMake project name.
+#   \optional[value] COMPONENT:component The name of the install
+#     component to generate the binary package from, defaults to
+#     ${REMAKE_DEFAULT_COMPONENT}.
+macro(remake_pack_binary pack_generator)
   remake_arguments(PREFIX pack_ VAR NAME VAR COMPONENT ${ARGN})
   remake_set(pack_component SELF DEFAULT ${REMAKE_DEFAULT_COMPONENT})
 
   remake_component_get(${pack_component} BUILD OUTPUT pack_build)
   if(pack_build)
-    if(NOT TARGET ${REMAKE_PACK_ALL_TARGET})
-      remake_target(${REMAKE_PACK_ALL_TARGET})
-    endif(NOT TARGET ${REMAKE_PACK_ALL_TARGET})
+    if(NOT TARGET ${REMAKE_PACK_ALL_BINARY_TARGET})
+      remake_target(${REMAKE_PACK_ALL_BINARY_TARGET})
+    endif(NOT TARGET ${REMAKE_PACK_ALL_BINARY_TARGET})
 
     remake_set(pack_name SELF DEFAULT ${REMAKE_PROJECT_NAME})
     remake_set(pack_component SELF DEFAULT ${REMAKE_DEFAULT_COMPONENT})
@@ -64,11 +68,9 @@ macro(remake_pack pack_generator)
     else(pack_component STREQUAL REMAKE_DEFAULT_COMPONENT)
       remake_set(pack_prefix ${pack_component})
     endif(pack_component STREQUAL REMAKE_DEFAULT_COMPONENT)
-    remake_file(pack_config ${REMAKE_PACK_DIR}/${pack_component}.cpack)
-    remake_file(pack_src_config
-      ${REMAKE_PACK_SOURCE_DIR}/${pack_component}.cpack)
+    remake_file(pack_config
+      ${REMAKE_PACK_DIR}/${pack_generator}/${pack_component}.cpack)
     remake_set(CPACK_OUTPUT_CONFIG_FILE ${pack_config})
-    remake_set(CPACK_SOURCE_OUTPUT_CONFIG_FILE ${pack_src_config})
 
     remake_set(CPACK_GENERATOR ${pack_generator})
     remake_set(CPACK_INSTALL_CMAKE_PROJECTS ${CMAKE_BINARY_DIR}
@@ -88,37 +90,88 @@ macro(remake_pack pack_generator)
       "${REMAKE_PROJECT_ADMIN} <${REMAKE_PROJECT_CONTACT}>")
 
     if(${pack_component} STREQUAL ${REMAKE_DEFAULT_COMPONENT})
-      message(STATUS "Package: ${pack_name} (${pack_generator})")
+      message(STATUS "Binary package: ${pack_name} (${pack_generator})")
     else(${pack_component} STREQUAL ${REMAKE_DEFAULT_COMPONENT})
-      message(STATUS "Package: ${pack_name}, "
+      message(STATUS "Binary package: ${pack_name}, "
         "using component ${pack_component} (${pack_generator})")
     endif(${pack_component} STREQUAL ${REMAKE_DEFAULT_COMPONENT})
 
     remake_unset(CPack_CMake_INCLUDED)
     include(CPack)
 
-    remake_target_name(pack_target ${pack_prefix} ${REMAKE_PACK_TARGET_SUFFIX})
-    remake_target(${pack_target}
+    remake_target_name(pack_target ${pack_prefix}
+      ${REMAKE_PACK_BINARY_TARGET_SUFFIX})
+    if(NOT TARGET ${pack_target})
+      remake_target(${pack_target})
+    endif(NOT TARGET ${pack_target})
+    remake_target_add_command(${pack_target}
       COMMAND cpack --config ${pack_config}
-      COMMENT "Building ${pack_name} package")
+      COMMENT "Building ${pack_name} binary package")
     remake_component_add_dependencies(
       COMPONENT ${pack_component}
       PROVIDES ${pack_target})
-    add_dependencies(${REMAKE_PACK_ALL_TARGET} ${pack_target})
+    add_dependencies(${REMAKE_PACK_ALL_BINARY_TARGET} ${pack_target})
 
     remake_var_regex(pack_variables "^CPACK_")
     foreach(pack_var ${pack_variables})
       remake_set(${pack_var})
     endforeach(pack_var)
   endif(pack_build)
-endmacro(remake_pack)
+endmacro(remake_pack_binary)
 
-### \brief Generate a Debian package from the ReMake project.
+### \brief Generate source packages from a ReMake project.
+#   This macro generally configures source package generation for a
+#   ReMake project using CMake's CPack macros. It gets invoked by the
+#   generator-specific macros defined in this module and should not be
+#   called directly from a CMakeLists.txt file. The macro creates a
+#   project-wide package build target and initializes the CPack variables.
+#   Assuming an out-of-source build, ${CMAKE_BINARY_DIR} is automatically
+#   excluded from the package along with any hidden files or directories.
+#   \required[value] generator The generator to be used for creating the
+#     source package. See the CPack documentation for valid generators.
+#   \optional[value] NAME:name The name of the source package to be
+#     generated, defaults to the ReMake project name.
+macro(remake_pack_source pack_generator)
+  remake_arguments(PREFIX pack_ VAR NAME ${ARGN})
+
+  if(NOT TARGET ${REMAKE_PACK_ALL_SOURCE_TARGET})
+    remake_target(${REMAKE_PACK_ALL_SOURCE_TARGET})
+  endif(NOT TARGET ${REMAKE_PACK_ALL_SOURCE_TARGET})
+
+  remake_set(pack_name SELF DEFAULT ${REMAKE_PROJECT_NAME})
+
+  remake_file(pack_src_config
+    ${REMAKE_PACK_SOURCE_DIR}/${pack_generator}/source.cpack)
+  remake_set(CPACK_SOURCE_OUTPUT_CONFIG_FILE ${pack_src_config})
+  remake_set(CPACK_SOURCE_GENERATOR ${pack_generator})
+  remake_set(CPACK_PACKAGE_NAME ${pack_name})
+  remake_set(CPACK_PACKAGE_VERSION ${REMAKE_PROJECT_VERSION})
+
+  file(RELATIVE_PATH pack_binary_dir ${CMAKE_SOURCE_DIR} ${CMAKE_BINARY_DIR})
+  remake_set(CPACK_SOURCE_IGNORE_FILES
+    "/\\\\\\\\\\\\\\\\..*/;/${pack_binary_dir}/")
+
+  message(STATUS "Source package: ${pack_name} (${pack_generator})")
+
+  remake_unset(CPack_CMake_INCLUDED)
+  include(CPack)
+
+  remake_target_add_command(${REMAKE_PACK_ALL_SOURCE_TARGET}
+    COMMAND cpack --config ${pack_src_config}
+    COMMENT "Building ${pack_name} source package")
+
+  remake_var_regex(pack_variables "^CPACK_")
+  foreach(pack_var ${pack_variables})
+    remake_set(${pack_var})
+  endforeach(pack_var)
+endmacro(remake_pack_source)
+
+### \brief Generate a binary Debian package from the ReMake project.
 #   This macro configures package generation using CPack's DEB generator
-#   for Debian packages. It acquires all the information necessary from
-#   the current project settings and the arguments passed. In addition to
-#   creating a package build target through remake_pack(), the macro adds
-#   simplified package install and uninstall targets. Project-internal
+#   for binary Debian packages. It acquires all the information necessary
+#   from the current project settings and the arguments passed. In addition
+#   to creating a package build target through remake_pack(), the macro
+#   adds simplified package install and uninstall targets. Project-internal
 #   dependencies between these targets are automatically resolved. Also,
 #   the macro provides automated resolution of package dependencies with
 #   hardcoded version information, i.e. packages including the version
@@ -210,9 +263,10 @@ macro(remake_pack_deb)
     remake_set(CPACK_DEBIAN_PACKAGE_ARCHITECTURE ${pack_arch})
     remake_set(CPACK_PACKAGE_FILE_NAME ${pack_file})
 
-    remake_pack(DEB COMPONENT ${pack_component} NAME ${pack_name})
+    remake_pack_binary(DEB COMPONENT ${pack_component} NAME ${pack_name})
 
-    remake_target_name(pack_target ${pack_prefix} ${REMAKE_PACK_TARGET_SUFFIX})
+    remake_target_name(pack_target ${pack_prefix}
+      ${REMAKE_PACK_BINARY_TARGET_SUFFIX})
     remake_target_name(pack_install_target ${pack_prefix}
       ${REMAKE_PACK_INSTALL_TARGET_SUFFIX})
     remake_target(${pack_install_target}
@@ -246,3 +300,75 @@ macro(remake_pack_deb)
     endforeach(pack_component_dep)
   endif(pack_build)
 endmacro(remake_pack_deb)
+
+### \brief Generate a binary archive from the ReMake project.
+#   This macro configures binary package generation using one of CPack's
+#   archive generators. It acquires all the information necessary from the
+#   current project settings and the arguments passed.
+#   \optional[value] GENERATOR:generator The generator to be used for creating
+#     the binary archive, defaults to TGZ. See the CPack documentation for valid
+#     archive generators.
+#   \optional[value] NAME:name The name of the binary package to be generated,
+#     defaults to the ReMake project name.
+#   \optional[value] ARCH:architecture The architecture that is appended to
+#     the archive name, defaults to the local system architecture as returned
+#     by 'uname -m'.
+#   \optional[value] COMPONENT:component The name of the install component to
+#     generate the binary archive from, defaults to ${REMAKE_DEFAULT_COMPONENT}.
+#     Note that the component name is used as suffix to the package name.
+#     However, a component name matching ${REMAKE_DEFAULT_COMPONENT} results
+#     in an empty suffix.
+macro(remake_pack_archive)
+  remake_arguments(PREFIX pack_ VAR GENERATOR VAR ARCH VAR COMPONENT ${ARGN})
+  remake_set(pack_generator SELF DEFAULT TGZ)
+  remake_set(pack_component SELF DEFAULT ${REMAKE_DEFAULT_COMPONENT})
+
+  remake_component_get(${pack_component} BUILD OUTPUT pack_build)
+  if(pack_build)
+    execute_process(COMMAND uname -m
+      OUTPUT_VARIABLE pack_uname_arch OUTPUT_STRIP_TRAILING_WHITESPACE)
+    remake_set(pack_arch SELF DEFAULT ${pack_uname_arch})
+    if(pack_component MATCHES "^${REMAKE_DEFAULT_COMPONENT}[-]?.*$")
+      string(REGEX REPLACE "^(${REMAKE_DEFAULT_COMPONENT})[-]?(.*)$" "\\2"
+        pack_prefix ${pack_component})
+    else(pack_component MATCHES "^${REMAKE_DEFAULT_COMPONENT}[-]?.*$")
+      remake_set(pack_prefix ${pack_component})
+    endif(pack_component MATCHES "^${REMAKE_DEFAULT_COMPONENT}[-]?.*$")
+    remake_set(pack_suffix ${pack_prefix})
+
+    if(pack_suffix)
+      remake_file_name(pack_name ${REMAKE_PROJECT_FILENAME}-${pack_suffix})
+      remake_file_name(pack_file ${REMAKE_PROJECT_FILENAME}-${pack_suffix}
+        ${REMAKE_PROJECT_VERSION} ${pack_arch})
+    else(pack_suffix)
+      remake_file_name(pack_name ${REMAKE_PROJECT_FILENAME})
+      remake_file_name(pack_file ${REMAKE_PROJECT_FILENAME}
+        ${REMAKE_PROJECT_VERSION} ${pack_arch})
+    endif(pack_suffix)
+
+    remake_set(CPACK_PACKAGE_FILE_NAME ${pack_file})
+    remake_pack_binary(${pack_generator} COMPONENT ${pack_component}
+      NAME ${pack_name})
+  endif(pack_build)
+endmacro(remake_pack_archive)
+
+### \brief Generate a source archive from the ReMake project.
+#   This macro configures source package generation using one of CPack's
+#   archive generators. It acquires all the information necessary from the
+#   current project settings and the arguments passed.
+#   \optional[value] GENERATOR:generator The generator to be used for creating
+#     the source archive, defaults to TGZ. See the CPack documentation for valid
+#     archive generators.
+#   \optional[value] NAME:name The name of the source package to be generated,
+#     defaults to the ReMake project name.
+macro(remake_pack_source_archive)
+  remake_arguments(PREFIX pack_ VAR GENERATOR ${ARGN})
+  remake_set(pack_generator SELF DEFAULT TGZ)
+
+  remake_file_name(pack_name ${REMAKE_PROJECT_FILENAME})
+  remake_file_name(pack_file ${REMAKE_PROJECT_FILENAME}
+    ${REMAKE_PROJECT_VERSION})
+
+  remake_set(CPACK_SOURCE_PACKAGE_FILE_NAME ${pack_file})
+  remake_pack_source(${pack_generator} NAME ${pack_name})
+endmacro(remake_pack_source_archive)
--- src/ReMakeROS.cmake
+++ src/ReMakeROS.cmake
@@ -43,9 +43,11 @@ endmacro(remake_ros)
 #   This macro discovers a ROS package in the distribution under ${ROS_PATH}
 #   by calling rospack. If the ROS package was found, the variable name
 #   conversion of ROS_${PACKAGE}_FOUND is set to TRUE, and ROS_${PACKAGE}_PATH,
-#   ROS_${PACKAGE}_INCLUDE_DIRS, and ROS_${PACKAGE}_LIBRARIES are initialized
-#   accordingly. In addition, the directories in which the linker will look for
-#   the package libraries is specified by calling CMake's link_directories().
+#   ROS_${PACKAGE}_INCLUDE_DIRS, ROS_${PACKAGE}_LIBRARIES, and
+#   ROS_${PACKAGE}_LIBRARY_DIRS are initialized accordingly. In addition, the
+#   directories in which the linker will look for the package libraries is
+#   specified by passing ROS_${PACKAGE}_LIBRARY_DIRS to CMake's
+#   link_directories().
 #   \required[value] package The name of the ROS package to be discovered.
 #   \optional[option] OPTIONAL If provided, this option is passed on to
 #     remake_find_result().
@@ -57,8 +59,9 @@ macro(remake_ros_find_package ros_package)
     remake_find_executable(rospack PATHS $ENV{ROS_ROOT}/../../bin)
 
     remake_var_name(ros_path_var ROS ${ros_package} PATH)
-    remake_var_name(ros_includes_var ROS ${ros_package} INCLUDE_DIRS)
+    remake_var_name(ros_include_dirs_var ROS ${ros_package} INCLUDE_DIRS)
     remake_var_name(ros_libraries_var ROS ${ros_package} LIBRARIES)
+    remake_var_name(ros_library_dirs_var ROS ${ros_package} LIBRARY_DIRS)
 
     execute_process(
       COMMAND ${ROSPACK_EXECUTABLE} find ${ros_package}
@@ -72,7 +75,7 @@ macro(remake_ros_find_package ros_package)
     else(ros_result)
       execute_process(
         COMMAND ${ROSPACK_EXECUTABLE} cflags-only-I ${ros_package}
-        OUTPUT_VARIABLE ros_includes
+        OUTPUT_VARIABLE ros_include_dirs
         OUTPUT_STRIP_TRAILING_WHITESPACE)
       execute_process(
         COMMAND ${ROSPACK_EXECUTABLE} libs-only-l ${ros_package}
@@ -80,17 +83,18 @@ macro(remake_ros_find_package ros_package)
         OUTPUT_STRIP_TRAILING_WHITESPACE)
       execute_process(
         COMMAND ${ROSPACK_EXECUTABLE} libs-only-L ${ros_package}
-        OUTPUT_VARIABLE ros_link_directories
+        OUTPUT_VARIABLE ros_library_dirs
         OUTPUT_STRIP_TRAILING_WHITESPACE)
 
       remake_set(${ros_path_var} ${${ros_path_var}} CACHE PATH
         "Path to ROS package ${ros_package}.")
-      string(REGEX REPLACE "[ ]+" ";" ${ros_includes_var} ${ros_includes})
+      string(REGEX REPLACE "[ ]+" ";" ${ros_include_dirs_var}
+        ${ros_include_dirs})
       string(REGEX REPLACE "[ ]+" ";" ${ros_libraries_var} ${ros_libraries})
-      string(REGEX REPLACE "[ ]+" ";" ros_link_directories
-        ${ros_link_directories})
+      string(REGEX REPLACE "[ ]+" ";" ${ros_library_dirs_var}
+        ${ros_library_dirs})
 
-      link_directories(${ros_link_directories})
+      link_directories(${${ros_library_dirs_var}})
     endif(ros_result)
 
     remake_find_result("ROS ${ros_package}" ${${ros_path_var}} ${OPTIONAL})
